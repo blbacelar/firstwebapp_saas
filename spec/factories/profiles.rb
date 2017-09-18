68c35a4b69990ed4dccd21ec835062d540bb8643
Bruno Bacelar <blbacelar@gmail.com>
2017-09-18 15:46:52 -0700

    Added profile model and table

---
+++ spec/factories/profiles.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :profile do
+    user_id 1
+    first_name "MyString"
+    last_name "MyString"
+    job_title "MyString"
+    phone_number "MyString"
+    contact_email "MyString"
+    description "MyText"
+  end
+end
